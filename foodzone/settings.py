82cb98c2b8d209e5ca841a644c8f2e84fd5ffff6
Unetha <94890052+Unetha@users.noreply.github.com>
2023-05-01 21:18:38 +0530

    Update settings.py

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = [ 
