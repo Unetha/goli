d7064102ebfb675d1abdf7ca1ee0aa23be8e1f05
Unetha <94890052+Unetha@users.noreply.github.com>
2023-05-04 16:59:43 +0530

    Update urls.py

--- foodzone/urls.py
+++ foodzone/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from myapp import views 
 from django.conf import settings 
 from django.conf.urls.static import static
