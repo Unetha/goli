7d886f6622c5421ad1abf3994d788744a62c8944
Unetha <94890052+Unetha@users.noreply.github.com>
2023-05-04 16:52:05 +0530

    Update urls.py

--- foodzone/urls.py
+++ foodzone/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('uttej', admin.site.urls),
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     path('contact/',views.contact_us,name="contact"),
