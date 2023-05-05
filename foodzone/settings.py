c139a7e9e33afb1b68bbf3ea278f4f7f3b90492e
Unetha <94890052+Unetha@users.noreply.github.com>
2023-05-05 11:42:04 +0530

    Update settings.py

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -3,6 +3,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
+PROJECT_ROOT=os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
